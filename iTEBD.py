b6eb3d5d935ab64582423c82d458a2c72bcc739a
kaihsinwu <kaihsinwu@gmail.com>
2019-01-01 09:52:35 +0800

    iTEBD has bug. Fix time evolution OP

--- iTEBD.py
+++ iTEBD.py
@@ -1,13 +1,27 @@
+import sys
+sys.path.append("..")
 import Tor10 as Tt
 import torch as tor
 import copy
 import numpy as np 
 
 
+if len(sys.argv) < 5:
+    print(".py <J> <Hx> <chi> <converge>")
+    exit(1)
+
 ## Params H = [J]SzSz - [Hx]Sx
-chi = 4
-Hx  = 2.5
-J   = -1.
+chi = int(sys.argv[3])
+Hx  = float(sys.argv[2])
+J   = float(sys.argv[1])
+CvgCrit = float(sys.argv[4])
+
+## check:
+if(chi<1):
+    raise ValueError("[ERROR] bond dimension should be >=1")
+if(CvgCrit<=0):
+    raise ValueError("[ERROR] converge should be >0")
+
 
 ## Create onsite-Op.
 Sz = Tt.UniTensor(bonds=[Tt.Bond(Tt.BD_IN,2),Tt.Bond(Tt.BD_OUT,2)],dtype=tor.float64,device=tor.device("cpu"))
@@ -28,8 +42,8 @@ Sx = Sx*Hx
 I.SetLabels([4,5])
 
 
-print(Sx)
-print(I)
+#print(Sx)
+#print(I)
 TFterm = Tt.Contract(Sx,I) + Tt.Contract(I,Sx)
 # del Sx, I
 
@@ -44,7 +58,7 @@ H.Contiguous()
 H.Reshape([4,4],new_labels=[0,1],N_inbond=1)
 
 ## Create Evov Op.
-eH = Tt.ExpH(H)
+eH = Tt.ExpH(H*-0.1)
 eH.Reshape([2,2,2,2],new_labels=[0,1,2,3],N_inbond=2)
 H.Reshape([2,2,2,2],new_labels=[0,1,2,3],N_inbond=2) # this is estimator.
 
@@ -69,59 +83,68 @@ lb.Rand()
 la.Rand()
 
 ## Evov:
+Elast = 0
+for i in range(100000):
+    A.SetLabels([-1,0,-2])
+    B.SetLabels([-3,1,-4])
+    la.SetLabels([-2,-3])
+    lb.SetLabels([-4,-5])
 
-for i in range(100):
-	A.SetLabels([-1,0,-2])
-	B.SetLabels([-3,1,-4])
-	la.SetLabels([-2,-3])
-	lb.SetLabels([-4,-5])
+    X = Tt.Contract(Tt.Contract(A,la),Tt.Contract(B,lb))
+    lb.SetLabel(-1,idx=1)
+    X = Tt.Contract(lb,X)
 
-	X = Tt.Contract(Tt.Contract(A,la),Tt.Contract(B,lb))
-	lb.SetLabel(-1,idx=1)
-	X = Tt.Contract(lb,X)
+    ## X =
+    #           (0)  (1)
+    #            |    |     
+    #  (-4) --lb-A-la-B-lb-- (-5) 
+    #
+    
+    XNorm = Tt.Contract(X, X)
+    XH = Tt.Contract(X, H)
+    XH.SetLabels([-4,0,1,-5])
+    XHX = Tt.Contract(X, XH)
+    XeH = Tt.Contract(X,eH)
+    
+    # measurements
+    E = (XHX.Storage / XNorm.Storage).item()
+    if np.abs(E - Elast) < CvgCrit:
+        break
+    Elast = E
+    print("Energy = {:.6f}".format(E))
 
-	## X =
-	#           (0)  (1)
-	#            |    |     
-	#  (-4) --lb-A-la-B-lb-- (-5) 
-	#
-	
-	XNorm = Tt.Contract(X, X)
-	XH = Tt.Contract(X, H)
-	XH.SetLabels([-4,0,1,-5])
-	XHX = Tt.Contract(X, XH)
-	XeH = Tt.Contract(X,eH)
-	
-	# measurements
-	E = (XHX.Storage / XNorm.Storage).item()
-	print("Energy = {:.6f}".format(E))
+    XeH.Permute([-4,2,3,-5],N_inbond=2,by_label=True)
+    XeH.Contiguous()
+    XeH.Reshape([chi*2,chi*2],N_inbond=1)
 
-	XeH.Permute([-4,2,3,-5],N_inbond=2,by_label=True)
-	XeH.Contiguous()
-	XeH.Reshape([chi*2,chi*2],N_inbond=1)
+    A,la,B = Tt.Svd_truncate(XeH,chi)
 
-	A,la,B = Tt.Svd_truncate(XeH,chi)
+    la *= 1./la.Norm()
 
-	la *= 1./la.Norm()
+    A.Reshape([chi,2,chi], new_labels=[-1,0,-2], N_inbond=1)
+    B.Reshape([chi,2,chi], new_labels=[-3,1,-4], N_inbond=1)
 
-	A.Reshape([chi,2,chi], new_labels=[-1,0,-2], N_inbond=1)
-	B.Reshape([chi,2,chi], new_labels=[-3,1,-4], N_inbond=1)
-
-	# de-contract the lb tensor , so it returns to 
-	#             
-	#            |     |     
+    # de-contract the lb tensor , so it returns to 
+    #             
+    #            |     |     
 	#       --lb-A'-la-B'-lb-- 
 	#
 	# again, but A' and B' are updated 
 	
 
-	lb_inv = Tt.Inverse(lb)
-	A = Tt.Contract(lb_inv, A)
-	B = Tt.Contract(B, lb_inv)
+    lb_inv = Tt.Inverse(lb)
+    A = Tt.Contract(lb_inv, A)
+    B = Tt.Contract(B, lb_inv)
 
-	# translation symmetry, exchange A and B site
-	A,B = B,A
-	la,lb = lb,la 
+    ## Trainsion save:
+    #Tt.Save(A,"A")
+    #Tt.Save(B,"B")  
+    #Tt.Save(la,"la")
+    #Tt.Save(lb,"lb")
+
+    # translation symmetry, exchange A and B site
+    A,B = B,A
+    la,lb = lb,la 
 
 del X
 
