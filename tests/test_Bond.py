2e84aec71f07f9e026e7c1d76b6f719f23def084
Ying-Jer Kao <yingjerkao@gmail.com>
2019-06-20 11:51:08 -0400

    Change Tor10 to tor10 in tests

--- tests/test_Bond.py
+++ tests/test_Bond.py
@@ -1,5 +1,5 @@
 import unittest
-from context import Tor10
+from context import tor10
 
 
 class TestBondObjects(unittest.TestCase):
@@ -9,31 +9,31 @@ class TestBondObjects(unittest.TestCase):
         # =======================================
 
         # Non-symmetry:
-        self.bd_x = Tor10.Bond(3)
-        self.bd_y = Tor10.Bond(4)
-        self.bd_z = Tor10.Bond(3)
+        self.bd_x = tor10.Bond(3)
+        self.bd_y = tor10.Bond(4)
+        self.bd_z = tor10.Bond(3)
 
         # > U1
-        self.bd_sym_U1 = Tor10.Bond(3, qnums=[[-1], [0], [1]])
+        self.bd_sym_U1 = tor10.Bond(3, qnums=[[-1], [0], [1]])
 
         # > Z2
-        self.bd_sym_Z2 = Tor10.Bond(3, qnums=[[0], [1], [0]], sym_types=[Tor10.Symmetry.Zn(2)])
+        self.bd_sym_Z2 = tor10.Bond(3, qnums=[[0], [1], [0]], sym_types=[tor10.Symmetry.Zn(2)])
 
         # > Z4
-        self.bd_sym_Z4 = Tor10.Bond(3, qnums=[[0], [2], [3]], sym_types=[Tor10.Symmetry.Zn(4)])
+        self.bd_sym_Z4 = tor10.Bond(3, qnums=[[0], [2], [3]], sym_types=[tor10.Symmetry.Zn(4)])
 
         # > Multiple U1
-        self.bd_sym_multU1 = Tor10.Bond(3, qnums=[[-2, -1, 0, -1],
+        self.bd_sym_multU1 = tor10.Bond(3, qnums=[[-2, -1, 0, -1],
                                                   [1, -4, 0, 0],
                                                   [-8, -3, 1, 5]])
 
         # > Multiple mix symmetry: U1 x Z2 x Z4
-        self.bd_sym_mix = Tor10.Bond(3, qnums=[[-2, 0, 0],
+        self.bd_sym_mix = tor10.Bond(3, qnums=[[-2, 0, 0],
                                                [-1, 1, 3],
                                                [1, 0, 2]],
-                                     sym_types=[Tor10.Symmetry.U1(),
-                                                Tor10.Symmetry.Zn(2),
-                                                Tor10.Symmetry.Zn(4)])
+                                     sym_types=[tor10.Symmetry.U1(),
+                                                tor10.Symmetry.Zn(2),
+                                                tor10.Symmetry.Zn(4)])
 
     def test_equal(self):
         self.assertEqual(self.bd_x, self.bd_z)
@@ -43,28 +43,28 @@ class TestBondObjects(unittest.TestCase):
         self.assertFalse(self.bd_x is self.bd_z)
 
     def test_assign(self):
-        bond1=Tor10.Bond(3)
+        bond1=tor10.Bond(3)
         bond1.assign(5)
         self.assertEqual(bond1.dim, 5)
         bond1.assign(3,qnums=[[1], [2], [3]])
         self.assertListEqual(list(bond1.qnums), [[1], [2], [3]])
 
 
-        bond1.assign(3, qnums=[[1], [2], [3]], sym_types=[Tor10.Symmetry.Zn(4)])
-        self.assertEqual(bond1.sym_types,[Tor10.Symmetry.Zn(4)] )
+        bond1.assign(3, qnums=[[1], [2], [3]], sym_types=[tor10.Symmetry.Zn(4)])
+        self.assertEqual(bond1.sym_types,[tor10.Symmetry.Zn(4)] )
 
     def test_change(self):
-        bond1=Tor10.Bond(3)
-        bond1.change(Tor10.BD_REGULAR)
-        self.assertEqual(bond1.bondType,Tor10.BD_REGULAR)
+        bond1=tor10.Bond(3)
+        bond1.change(tor10.BD_REGULAR)
+        self.assertEqual(bond1.bondType,tor10.BD_REGULAR)
 
     def test_combine(self):
 
-        a = Tor10.Bond(3)
-        b = Tor10.Bond(4)
-        c = Tor10.Bond(2, qnums=[[0, 1, -1], [1, 1, 0]])
-        d = Tor10.Bond(2, qnums=[[1, 0, -1], [1, 0, 0]])
-        e = Tor10.Bond(2, qnums=[[1, 0], [1, 0]], sym_types=[Tor10.Symmetry.U1(),Tor10.Symmetry.Zn(4)])
+        a = tor10.Bond(3)
+        b = tor10.Bond(4)
+        c = tor10.Bond(2, qnums=[[0, 1, -1], [1, 1, 0]])
+        d = tor10.Bond(2, qnums=[[1, 0, -1], [1, 0, 0]])
+        e = tor10.Bond(2, qnums=[[1, 0], [1, 0]], sym_types=[tor10.Symmetry.U1(),tor10.Symmetry.Zn(4)])
 
         a.combine(b)
         self.assertEqual(a.dim, 12)
--- tests/test_UniTensor.py
+++ tests/test_UniTensor.py
@@ -1,63 +1,63 @@
 import unittest
-from context import Tor10
+from context import tor10
 import numpy as np
 class TestUniTensorObjects(unittest.TestCase):
 
 #    def setUp(self):
-#        self.bds_x = [Tor10.Bond(5), Tor10.Bond(5), Tor10.Bond(3)]
-#        self.x = Tor10.UniTensor(bonds=self.bds_x, N_inbond=1, labels=[4, 3, 5])
+#        self.bds_x = [tor10.Bond(5), tor10.Bond(5), tor10.Bond(3)]
+#        self.x = tor10.UniTensor(bonds=self.bds_x, N_inbond=1, labels=[4, 3, 5])
 
     def test_SetLabel(self):
-        bds_x = [Tor10.Bond(5), Tor10.Bond(5), Tor10.Bond(3)]
-        x = Tor10.UniTensor(bonds=bds_x, rowrank=1, labels=[4, 3, 5])
+        bds_x = [tor10.Bond(5), tor10.Bond(5), tor10.Bond(3)]
+        x = tor10.UniTensor(bonds=bds_x, rowrank=1, labels=[4, 3, 5])
         x.SetLabel(-1, 2)
         self.assertEqual(x.labels[2],-1)
 
     def test_SetLabels(self):
-        bds_x = [Tor10.Bond(5), Tor10.Bond(5), Tor10.Bond(3)]
-        x = Tor10.UniTensor(bonds=bds_x, rowrank=1, labels=[4, 3, 5])
+        bds_x = [tor10.Bond(5), tor10.Bond(5), tor10.Bond(3)]
+        x = tor10.UniTensor(bonds=bds_x, rowrank=1, labels=[4, 3, 5])
         x.SetLabels([3,2,1])
         self.assertListEqual(list(x.labels),[3,2,1])
 
     def test_Reshape(self):
-        bds_x = [Tor10.Bond(6), Tor10.Bond(5), Tor10.Bond(3)]
-        x = Tor10.UniTensor(bonds=bds_x, rowrank=1, labels=[4, 3, 5])
+        bds_x = [tor10.Bond(6), tor10.Bond(5), tor10.Bond(3)]
+        x = tor10.UniTensor(bonds=bds_x, rowrank=1, labels=[4, 3, 5])
         y=x.Reshape([2, 3, 5, 3], new_labels=[1, 2, 3, -1], rowrank=2)
         self.assertListEqual(list(y.labels),[1, 2, 3, -1])
 
     def test_Reshape_(self):
-        bds_x = [Tor10.Bond(6), Tor10.Bond(5), Tor10.Bond(3)]
-        x = Tor10.UniTensor(bonds=bds_x, rowrank=1, labels=[4, 3, 5])
+        bds_x = [tor10.Bond(6), tor10.Bond(5), tor10.Bond(3)]
+        x = tor10.UniTensor(bonds=bds_x, rowrank=1, labels=[4, 3, 5])
         x.Reshape_([2, 3, 5, 3], new_labels=[1, 2, 3, -1], rowrank=2)
         self.assertListEqual(list(x.labels), [1, 2, 3, -1])
 
     def test_CombineBonds(self):
-        bds_x = [Tor10.Bond(5), Tor10.Bond(5), Tor10.Bond(3)]
-        x = Tor10.UniTensor(bonds=bds_x, rowrank=2, labels=[4, 3, 5])
+        bds_x = [tor10.Bond(5), tor10.Bond(5), tor10.Bond(3)]
+        x = tor10.UniTensor(bonds=bds_x, rowrank=2, labels=[4, 3, 5])
         x.CombineBonds([5, 3])
         self.assertListEqual(list(x.shape),[5,15])
         self.assertListEqual(list(x.labels), [4,5])
 
-        y = Tor10.UniTensor(bonds=bds_x, rowrank=2, labels=[4, 3, 5])
+        y = tor10.UniTensor(bonds=bds_x, rowrank=2, labels=[4, 3, 5])
         y.CombineBonds([3,5])
         self.assertListEqual(list(y.shape), [15, 5])
         self.assertListEqual(list(y.labels), [3, 4])
 
-        z = Tor10.UniTensor(bonds=bds_x * 2, rowrank=3, labels=[4, 3, 5, 6, 7, 8])
+        z = tor10.UniTensor(bonds=bds_x * 2, rowrank=3, labels=[4, 3, 5, 6, 7, 8])
         z.CombineBonds([4, 5, 6])
         self.assertListEqual(list(z.shape), [75, 5, 5, 3])
         self.assertListEqual(list(z.labels), [4, 3, 7, 8])
         self.assertEqual(z.rowrank, 1)
 
-        z2 = Tor10.UniTensor(bonds=bds_x * 2, rowrank=3, labels=[4, 3, 5, 6, 7, 8])
+        z2 = tor10.UniTensor(bonds=bds_x * 2, rowrank=3, labels=[4, 3, 5, 6, 7, 8])
         z2.CombineBonds([4,5,6],permute_back=True)
         self.assertListEqual(list(z2.shape), [75, 5, 5, 3])
         self.assertListEqual(list(z2.labels), [4, 3, 7, 8])
         self.assertEqual(z2.rowrank, 2)
 
     def test_Permute(self):
-        bds_x = [Tor10.Bond(6), Tor10.Bond(5), Tor10.Bond(4), Tor10.Bond(3), Tor10.Bond(2)]
-        x = Tor10.UniTensor(bonds=bds_x, rowrank=3, labels=[1, 3, 5, 7, 8])
+        bds_x = [tor10.Bond(6), tor10.Bond(5), tor10.Bond(4), tor10.Bond(3), tor10.Bond(2)]
+        x = tor10.UniTensor(bonds=bds_x, rowrank=3, labels=[1, 3, 5, 7, 8])
         x.Permute([0,2,1,4,3])
         self.assertListEqual(list(x.labels),[1, 5, 3, 8, 7])
         self.assertEqual(x.rowrank, 3)
@@ -65,7 +65,7 @@ class TestUniTensorObjects(unittest.TestCase):
         self.assertFalse(x.is_contiguous())
 
 
-        y=Tor10.UniTensor(bonds=bds_x, rowrank=3, labels=[1, 3, 5, 7, 8])
+        y=tor10.UniTensor(bonds=bds_x, rowrank=3, labels=[1, 3, 5, 7, 8])
         y.Permute([3,1,5,7,8],by_label=True)
         self.assertListEqual(list(y.labels),[3, 1, 5, 7, 8])
         self.assertEqual(y.rowrank, 3)
@@ -73,7 +73,7 @@ class TestUniTensorObjects(unittest.TestCase):
         self.assertFalse(y.is_contiguous())
 
 
-        z=Tor10.UniTensor(bonds=bds_x, rowrank=3, labels=[1, 3, 5, 7, 8])
+        z=tor10.UniTensor(bonds=bds_x, rowrank=3, labels=[1, 3, 5, 7, 8])
         z.Permute([3, 1, 5, 7, 8], rowrank=2, by_label=True)
         self.assertListEqual(list(z.labels),[3, 1, 5, 7, 8])
         self.assertEqual(z.rowrank,2)
@@ -84,21 +84,21 @@ class TestUniTensorObjects(unittest.TestCase):
         pass
 
     def test_GetTotalQnums(self):
-        bd_sym_1 = Tor10.Bond(3, Tor10.BD_KET, qnums=[[0, 2, 1, 0],
+        bd_sym_1 = tor10.Bond(3, tor10.BD_KET, qnums=[[0, 2, 1, 0],
                                                     [1, 1, -1, 1],
                                                     [2, -1, 1, 0]])
-        bd_sym_2 = Tor10.Bond(4, Tor10.BD_KET, qnums=[[-1, 0, -1, 3],
+        bd_sym_2 = tor10.Bond(4, tor10.BD_KET, qnums=[[-1, 0, -1, 3],
                                                     [0, 0, -1, 2],
                                                     [1, 0, 1, 0],
                                                     [2, -2, -1, 1]])
-        bd_sym_3 = Tor10.Bond(2, Tor10.BD_BRA, qnums=[[-4, 3, 0, -1],
+        bd_sym_3 = tor10.Bond(2, tor10.BD_BRA, qnums=[[-4, 3, 0, -1],
                                                     [1, 1, -2, 3]])
 
-        sym_T = Tor10.UniTensor(bonds=[bd_sym_1, bd_sym_2, bd_sym_3], rowrank=2, labels=[1, 2, 3])
+        sym_T = tor10.UniTensor(bonds=[bd_sym_1, bd_sym_2, bd_sym_3], rowrank=2, labels=[1, 2, 3])
 
         tqin, tqout = sym_T.GetTotalQnums()
 
-        qin=Tor10.Bond(12,Tor10.BD_KET, qnums=[[+4, -3, +0, +1], [+3, -1, +2, +0], [+2, -1, +0, +2],
+        qin=tor10.Bond(12,tor10.BD_KET, qnums=[[+4, -3, +0, +1], [+3, -1, +2, +0], [+2, -1, +0, +2],
                                                [+1, -1, +0, +3], [+3, -1, -2, +2], [+2, +1, +0, +1],
                                                [+1, +1, -2, +3], [+0, +1, -2, +4], [+2, +0, +0, +1],
                                                [+1, +2, +2, +0], [+0, +2, +0, +2], [-1, +2, +0, +3]]
